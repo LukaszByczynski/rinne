2fdf7479c0646bb10ac05bdb38b06bc277f2e27d
Lukasz Byczynski <mail@lukaszbyczynski.com>
2015-05-06 21:17:46 +0200

    Add listeners to VAbsoluteLayout.scala

--- addon/src/main/scala/org/vaadin/addons/rinne/VAbsoluteLayout.scala
+++ addon/src/main/scala/org/vaadin/addons/rinne/VAbsoluteLayout.scala
@@ -1,9 +1,10 @@
 package org.vaadin.addons.rinne
 
 import com.vaadin.ui.{AbsoluteLayout, Component}
-import org.vaadin.addons.rinne.mixins.AbstractLayoutMixin
+import org.vaadin.addons.rinne.mixins.{AbstractLayoutMixin, ComponentContainerMixin, ComponentMixin, LayoutClickNotifierMixin}
 
-class VAbsoluteLayout extends AbsoluteLayout with AbstractLayoutMixin {
+class VAbsoluteLayout extends AbsoluteLayout
+with AbstractLayoutMixin with ComponentContainerMixin with ComponentMixin with LayoutClickNotifierMixin {
 
   def add[C <: Component](component: C, location: String): C = {
     addComponent(component, location)
